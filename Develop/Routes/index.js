06cde82b41e8dfca1dfe758ddc353aba0ab4d1ff
Mario Repas <mario.repas63@gmail.com>
2023-02-15 20:01:40 -0500

    server is running and loads index.html on default

---
+++ Develop/Routes/index.js
@@ -0,0 +1,18 @@
+// This file will serve as a table of contents for all the routes in the application
+
+const express = require('express');
+const app = express();
+
+
+
+
+
+
+
+
+
+
+
+
+
+module.exports = app;
--- Develop/server.js
+++ Develop/server.js
@@ -1,6 +1,21 @@
 // set up server and listen on port 3001 for requests
 const express = require("express");
 const app = express();
-const port = 3001;
+const path = require("path");
+const api = require("./Routes/index");
+const PORT = 3001;
+
+// Default route to serve up the index.html page
+app.get("/", (req, res) =>
+    res.sendFile(path.join(__dirname, "./public/index.html"))
+);
 
 
+
+
+
+
+
+app.listen(PORT, () =>
+  console.log(`App listening at http://localhost:${PORT} 🚀`)
+);
